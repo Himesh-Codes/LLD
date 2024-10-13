1a98157d8bf8763666f23a130927df28f07ec592
Himesh S <himss@amazon.com>
2024-10-13 18:41:54 +0530

    refactor exceptions

--- Cron Expression Parser/src/core/cronfield.py
+++ Cron Expression Parser/src/core/cronfield.py
@@ -12,46 +12,52 @@ class CronField:
         Expands the cron field expression into a list of valid values.
         Supports wildcards (*), ranges, step values, and lists.
         """
-        try:
-            if self.field_expr == "*":
-                return list(range(self.min_value, self.max_value + 1))
-            elif "," in self.field_expr:
-                return self._expand_list()
-            elif "-" in self.field_expr and "/" not in self.field_expr:
-                return self._expand_range(self.field_expr)
-            elif "/" in self.field_expr:
-                return self._expand_step()
-            else:
-                return [int(self.field_expr)]
-        except Exception as e:
-            raise FieldExpansionError(e)
+        if self.field_expr == "*":
+            return list(range(self.min_value, self.max_value + 1))
+        elif "," in self.field_expr:
+            return self._expand_list()
+        elif "-" in self.field_expr and "/" not in self.field_expr:
+            return self._expand_range(self.field_expr)
+        elif "/" in self.field_expr:
+            return self._expand_step()
+        else:
+            return [int(self.field_expr)]
 
     def _expand_list(self):
         computed_set = set()
-        for x in self.field_expr.split(","):
-            if self.max_value >= x >= self.min_value:
-                computed_set.add(int(x))
-            else:
-                raise FieldExpansionError(f"Value {x} out of bounds for field value min: {self.min_value} and max: {self.max_value}")
+        try:
+            for x in self.field_expr.split(","):
+                if self.max_value >= x >= self.min_value:
+                    computed_set.add(int(x))
+                else:
+                    raise FieldExpansionError(f"Value {x} out of bounds for field value min: {self.min_value} and max: {self.max_value}")
+        except Exception as e:
+            raise FieldExpansionError(e)       
         return sorted(computed_set)
 
     def _expand_range(self, range_expr):
-        start, end = map(int, range_expr.split("-"))
-        if not(self.max_value >= start >= self.min_value and self.max_value >= end >= self.min_value):
-            raise FieldExpansionError(f"Value {start}/{end} out of bounds for field value min: {self.min_value} and max: {self.max_value}")
-        return list(range(start, end + 1))
-
-    def _expand_step(self):
-        if self.field_expr.startswith("*/"):
-            step = int(self.field_expr[2:])
-            return list(range(self.min_value, self.max_value + 1, step))
-        elif "-" in self.field_expr:
-            range_part, step = self.field_expr.split("/")
-            start, end = map(int, range_part.split("-"))
+        try:
+            start, end = map(int, range_expr.split("-"))
             if not(self.max_value >= start >= self.min_value and self.max_value >= end >= self.min_value):
                 raise FieldExpansionError(f"Value {start}/{end} out of bounds for field value min: {self.min_value} and max: {self.max_value}")
-            step = int(step)
-            return list(range(start, end + 1, step))
-        else:
-            step = int(self.field_expr.split("/")[1])
-            return list(range(int(self.field_expr.split("/")[0]), self.max_value + 1, step))
+            return list(range(start, end + 1))
+        except Exception as e:
+            raise FieldExpansionError(e)
+
+    def _expand_step(self):
+        try:
+            if self.field_expr.startswith("*/"):
+                step = int(self.field_expr[2:])
+                return list(range(self.min_value, self.max_value + 1, step))
+            elif "-" in self.field_expr:
+                range_part, step = self.field_expr.split("/")
+                start, end = map(int, range_part.split("-"))
+                if not(self.max_value >= start >= self.min_value and self.max_value >= end >= self.min_value):
+                    raise FieldExpansionError(f"Value {start}/{end} out of bounds for field value min: {self.min_value} and max: {self.max_value}")
+                step = int(step)
+                return list(range(start, end + 1, step))
+            else:
+                step = int(self.field_expr.split("/")[1])
+                return list(range(int(self.field_expr.split("/")[0]), self.max_value + 1, step))
+        except Exception as e:
+            raise FieldExpansionError(e)
